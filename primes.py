a161fcc3273276dd691ca70c866278b1f9a719f4
Roland Xhama <rxhama02@gmail.com>
2022-12-05 04:49:54 +0000

    Add first n prime solution

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    while(len(list) < number_of_primes):
+        if is_prime(num):
+            list.append(num)
+        num += 1
+
     return list
+
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
